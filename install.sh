3f0cf0afdffe4266a39d4f929c56a67f1d7ccfed
chgara <57034271+chgara@users.noreply.github.com>
2024-09-07 15:26:04 +0200

    working neovim

--- install.sh
+++ install.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-if [ "$EUID" -ne 0 ]; then
-  echo "Please run as root"
-  exit 1
-fi
-
 if command -v pacman &> /dev/null; then
-  pacman -S --noconfirm neovim xclip base-devel
+  sudo pacman -S --noconfirm neovim xclip base-devel
 elif command -v apt-get &> /dev/null; then
-  apt-get update && apt-get install -y neovim xclip build-essential
+  sudo apt-get update && apt-get install -y neovim xclip build-essential
 else
   echo "Unsupported package manager. Please install neovim, xclip, and build-essential manually."
   exit 1
